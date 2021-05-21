e9ed22ac47ad87e17dba8862232ad81feabd7dcf
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-05-21 12:41:20 +0200

    initial work to reinstate control plane mock

--- cos-fleetshard-mock/src/main/java/org/bf2/cos/fleetshard/cp/ControlPlaneMock.java
+++ cos-fleetshard-mock/src/main/java/org/bf2/cos/fleetshard/cp/ControlPlaneMock.java
@@ -1,161 +1,156 @@
 package org.bf2.cos.fleetshard.cp;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.Path;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.bf2.cos.fleet.manager.api.model.cp.ConnectorClusterStatus;
+import org.bf2.cos.fleet.manager.api.model.cp.ConnectorDeployment;
+import org.bf2.cos.fleet.manager.api.model.cp.ConnectorDeploymentList;
+import org.bf2.cos.fleet.manager.api.model.cp.ConnectorDeploymentStatus;
 
 @ApplicationScoped
-@Path("/api/managed-services-api/v1/kafka-connector-clusters")
+@Path("/api/connector_mgmt/v1/kafka-connector-clusters/{connector_cluster_id}")
 public class ControlPlaneMock {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ControlPlaneMock.class);
+    private final Map<String, ConnectorCluster> clusters;
 
-    //TODO: maybe reimplement
-    /*
-     * 
-     * private final ConnectorCluster cluster;
-     * private final AtomicLong counter;
-     * private final Map<String, ConnectorDeployment> connectors;
-     * 
-     * public ControlPlaneMock() {
-     * this.cluster = new ConnectorCluster();
-     * this.counter = new AtomicLong();
-     * this.connectors = new HashMap<>();
-     * }
-     * 
-     * public static void main(String[] args) {
-     * io.quarkus.runtime.Quarkus.run(args);
-     * }
-     * 
-     * @GET
-     * 
-     * @Path("/{cluster_id}")
-     * 
-     * @Produces(MediaType.APPLICATION_JSON)
-     * public ConnectorCluster getConnectorCluster(
-     * 
-     * @PathParam("id") String clusterId) {
-     * 
-     * return cluster;
-     * }
-     * 
-     * @PUT
-     * 
-     * @Path("/{cluster_id}/status")
-     * 
-     * @Consumes(MediaType.APPLICATION_JSON)
-     * 
-     * @Produces(MediaType.APPLICATION_JSON)
-     * public void updateKafkaConnectorClusterStatus(
-     * 
-     * @PathParam("cluster_id") String clusterId,
-     * ConnectorClusterStatus status) {
-     * 
-     * this.cluster.setStatus(status.getPhase());
-     * }
-     * 
-     * @GET
-     * 
-     * @Path("/{cluster_id}/deployments")
-     * 
-     * @Produces(MediaType.APPLICATION_JSON)
-     * public ConnectorDeploymentList getDeployments(
-     * 
-     * @PathParam("cluster_id") String clusterId,
-     * 
-     * @QueryParam("gt_version") long resourceVersion) {
-     * 
-     * List<ConnectorDeployment> deployments = connectors.values().stream()
-     * .filter(c -> resourceVersion < c.getMetadata().getResourceVersion())
-     * .sorted(Comparator.comparingLong(c -> c.getMetadata().getResourceVersion()))
-     * .collect(Collectors.toList());
-     * 
-     * return new ConnectorDeploymentList()
-     * .total(deployments.size())
-     * .page(0)
-     * .items(deployments);
-     * }
-     * 
-     * @GET
-     * 
-     * @Path("/{cluster_id}/connectors")
-     * 
-     * @Produces(MediaType.APPLICATION_JSON)
-     * public ConnectorDeploymentList getConnectors(
-     * 
-     * @PathParam("cluster_id") String clusterId) {
-     * 
-     * List<ConnectorDeployment> deployments = connectors.values().stream()
-     * .sorted(Comparator.comparingLong(c -> c.getMetadata().getResourceVersion()))
-     * .collect(Collectors.toList());
-     * 
-     * return new ConnectorDeploymentList()
-     * .total(deployments.size())
-     * .page(0)
-     * .items(deployments);
-     * }
-     * 
-     * @GET
-     * 
-     * @Path("/{id}/deployments/{deployment_id}")
-     * 
-     * @Produces(MediaType.APPLICATION_JSON)
-     * public ConnectorDeployment getDeployment(
-     * 
-     * @PathParam("cluster_id") String clusterId,
-     * 
-     * @PathParam("deployment_id") String deploymentId) {
-     * 
-     * return connectors.values().stream()
-     * .filter(cd -> Objects.equals(deploymentId, cd.getId()))
-     * .findFirst()
-     * .orElseThrow(() -> new IllegalArgumentException("Unknown deployment " + deploymentId));
-     * }
-     * 
-     * @PUT
-     * 
-     * @Path("/{cluster_id}/deployments/{deployment_id}/status")
-     * 
-     * @Consumes(MediaType.APPLICATION_JSON)
-     * 
-     * @Produces(MediaType.APPLICATION_JSON)
-     * public void updateConnector(
-     * 
-     * @PathParam("cluster_id") String clusterId,
-     * 
-     * @PathParam("deployment_id") String deploymentId,
-     * ConnectorDeploymentStatus status) throws JsonProcessingException {
-     * 
-     * LOGGER.info("Updating status {} -> {}",
-     * deploymentId,
-     * Serialization.jsonMapper().writerWithDefaultPrettyPrinter().writeValueAsString(status));
-     * 
-     * connectors.values().stream()
-     * .filter(cd -> Objects.equals(deploymentId, cd.getId()))
-     * .findFirst()
-     * .ifPresent(v -> v.setStatus(status));
-     * }
-     * 
-     * @POST
-     * 
-     * @Path("/{cluster_id}/connectors")
-     * 
-     * @Consumes(MediaType.APPLICATION_JSON)
-     * public void updateConnector(
-     * ConnectorDeployment connector) throws JsonProcessingException {
-     * 
-     * String id = connector.getId();
-     * long rev = counter.incrementAndGet();
-     * connector.setId(UUID.randomUUID().toString());
-     * connector.getMetadata().setResourceVersion(rev);
-     * connector.getSpec().setConnectorResourceVersion(rev);
-     * 
-     * LOGGER.info("Updating deployment {} ->{}",
-     * id,
-     * Serialization.jsonMapper().writerWithDefaultPrettyPrinter().writeValueAsString(connector));
-     * 
-     * connectors.put(id, connector);
-     * }
-     */
+    public ControlPlaneMock() {
+        this.clusters = new ConcurrentHashMap<>();
+    }
+
+    @GET
+    @Path("/deployments/{deployment_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ConnectorDeployment getConnectorDeployment(
+        @PathParam("connector_cluster_id") String connectorClusterId,
+        @PathParam("deployment_id") String deploymentId) {
+
+        return cluster(connectorClusterId).getConnectorDeployments(deploymentId);
+    }
+
+    @GET
+    @Path("/deployments")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ConnectorDeploymentList listConnectorDeployments(
+        @PathParam("connector_cluster_id") String connectorClusterId,
+        @QueryParam("page") String page,
+        @QueryParam("size") String size,
+        @QueryParam("gt_version") Long gtVersion,
+        @QueryParam("watch") String watch) {
+
+        return cluster(connectorClusterId).listConnectorDeployments();
+    }
+
+    @PUT
+    @Path("/deployments/{deployment_id}/status")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void updateConnectorDeploymentStatus(
+        @PathParam("connector_cluster_id") String connectorClusterId,
+        @PathParam("deployment_id") String deploymentId,
+        ConnectorDeploymentStatus connectorDeploymentStatus) {
+
+        cluster(connectorClusterId).updateConnectorDeploymentStatus(deploymentId, connectorDeploymentStatus);
+    }
+
+    @PUT
+    @Path("/status")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void updateClusterStatus(
+        @PathParam("connector_cluster_id") String connectorClusterId,
+        ConnectorClusterStatus connectorClusterStatus) {
+
+        cluster(connectorClusterId).updateConnectorClusterStatus(connectorClusterStatus);
+    }
+
+    // **********************************************
+    //
+    // Helper
+    //
+    // **********************************************
+
+    private ConnectorCluster cluster(String id) {
+        return clusters.computeIfAbsent(id, ConnectorCluster::new);
+    }
+
+    // **********************************************
+    //
+    // Model
+    //
+    // **********************************************
+
+    private static class ConnectorCluster {
+        private final String id;
+        private final Map<String, Connector> connectors;
+        private ConnectorClusterStatus status;
+
+        public ConnectorCluster(String id) {
+            this.id = id;
+            this.connectors = new ConcurrentHashMap<>();
+        }
+
+        public ConnectorDeployment getConnectorDeployments(String deploymentId) {
+            return connectors.containsKey(deploymentId)
+                ? connectors.get(deploymentId).getDeployment()
+                : null;
+        }
+
+        public ConnectorDeploymentList listConnectorDeployments() {
+            var items = connectors.values().stream()
+                .map(Connector::getDeployment)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+            return new ConnectorDeploymentList()
+                .page(1)
+                .size(items.size())
+                .total(items.size())
+                .items(items);
+        }
+
+        public void updateConnectorDeploymentStatus(String deploymentId, ConnectorDeploymentStatus connectorDeploymentStatus) {
+            connectors.computeIfPresent(deploymentId, (k, v) -> {
+                v.status = connectorDeploymentStatus;
+                return v;
+            });
+
+        }
+
+        public void updateConnectorClusterStatus(ConnectorClusterStatus connectorClusterStatus) {
+            this.status = connectorClusterStatus;
+        }
+    }
+
+    private static class Connector {
+        private ConnectorDeployment deployment;
+        private ConnectorDeploymentStatus status;
+
+        public ConnectorDeployment getDeployment() {
+            return deployment;
+        }
+
+        public void setDeployment(ConnectorDeployment deployment) {
+            this.deployment = deployment;
+        }
+
+        public ConnectorDeploymentStatus getStatus() {
+            return status;
+        }
+
+        public void setStatus(ConnectorDeploymentStatus status) {
+            this.status = status;
+        }
+    }
 }
