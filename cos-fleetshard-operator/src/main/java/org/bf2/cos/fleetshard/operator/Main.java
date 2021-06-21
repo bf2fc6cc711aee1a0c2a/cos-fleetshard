706b9b6f72b1c0e1502659d96177e37c5160c81f
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-06-21 11:04:43 +0200

    chore: add main entry point

---
+++ cos-fleetshard-operator/src/main/java/org/bf2/cos/fleetshard/operator/Main.java
@@ -0,0 +1,12 @@
+package org.bf2.cos.fleetshard.operator;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main {
+
+    public static void main(String ... args) {
+        Quarkus.run(args);
+    }
+}
