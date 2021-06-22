4faa6bab53c582abd260c7e9f8b33b5a160d7424
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-06-22 18:55:37 +0200

    ci: improve deploy job

--- etc/scripts/create-pull-secret.sh
+++ etc/scripts/create-pull-secret.sh
@@ -19,8 +19,3 @@ oc get secrets cos-pull-secret || oc create secret docker-registry cos-pull-secr
    --docker-password="${IMAGE_REPO_PASSWORD}" \
    --docker-email=mas-connectors@redhat.com
 
-oc apply -f etc/kubernetes/managedconnectoroperators.cos.bf2.org-v1.yml
-oc apply -f etc/kubernetes/managedconnectorclusters.cos.bf2.org-v1.yml
-oc apply -f etc/kubernetes/managedconnectors.cos.bf2.org-v1.yml
-#oc apply -f etc/kubernetes/kubernetes.yml
-#oc rollout restart deployment/cos-fleetshard
