6ec762ea5c9201a6d7aa4036af551b58bbb99d81
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-06-24 11:33:28 +0200

    chore: update utility scripts

--- etc/utils/get-clusters.sh
+++ etc/utils/get-clusters.sh
@@ -3,4 +3,6 @@
 BASE=${BASE_PATH}/api/connector_mgmt/v1
 CLUSTER_BASE=${BASE}/kafka_connector_clusters
 
-curl --insecure --oauth2-bearer "$(ocm token)" -S -s -D /dev/stderr "${CLUSTER_BASE}" 2>/dev/null | jq
+curl --insecure --oauth2-bearer "$(ocm token)" -S -s "${CLUSTER_BASE}" \
+    | jq -r '(["ID","NAME","OWNER","STATUS"] | (., map(length*"-"))), (.items[] | [.id, .metadata.name, .metadata.owner, .status]) | @tsv' \
+    | column -t -s $'\t' 
--- etc/utils/get-kafkas.sh
+++ etc/utils/get-kafkas.sh
@@ -3,4 +3,6 @@
 BASE=${BASE_PATH}/api/kafkas_mgmt/v1
 KAFKA_BASE=${BASE}/kafkas
 
-curl --insecure --oauth2-bearer "$(ocm token)" -S -s -D /dev/stderr "${KAFKA_BASE}" 2>/dev/null | jq
+curl --insecure --oauth2-bearer "$(ocm token)" -S -s "${KAFKA_BASE}"  \
+    | jq -r '(["ID","NAME","OWNER","STATUS"] | (., map(length*"-"))), (.items[] | [.id, .name, .owner, .status]) | @tsv' \
+    | column -t -s $'\t' 
