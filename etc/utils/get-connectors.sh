01ffd5d1fd95b4901c9fd165ab761dec444155ca
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-07-08 09:03:19 +0200

    chore: improve helper scripts

--- etc/utils/get-connectors.sh
+++ etc/utils/get-connectors.sh
@@ -4,5 +4,31 @@ BASE=${BASE_PATH}/api/connector_mgmt/v1
 CONNECTORS_BASE=${BASE}/kafka_connectors
 
 curl --insecure --oauth2-bearer "$(ocm token)" -S -s "${CONNECTORS_BASE}" \
-    | jq -r '(["ID","TYPE","OWNER","NAME","CLUSTER_ID","KAFKA_ID","DESIRED_STATE","STATUS"] | (., map(length*"-"))), (.items[]? | [.id, .connector_type_id, .metadata.owner, .metadata.name, .deployment_location.cluster_id, .metadata.kafka_id, .desired_state, .status]) | @tsv' \
+    | jq -r '
+            ([
+                "ID",
+                "TYPE",
+                "CHANNEL",
+                "OWNER",
+                "NAME",
+                "CLUSTER_ID",
+                "KAFKA_ID",
+                "CREATED_AT",
+                "DESIRED_STATE",
+                "STATUS"] 
+            | (., map(length*"-"))), 
+            (.items[]? | [
+                .id, 
+                .connector_type_id, 
+                .channel,
+                .metadata.owner, 
+                .metadata.name, 
+                .deployment_location.cluster_id, 
+                .metadata.kafka_id, 
+                .metadata.created_at,
+                .desired_state, 
+                .status
+            ]) 
+            | @tsv
+        ' \
     | column -t -s $'\t' 
