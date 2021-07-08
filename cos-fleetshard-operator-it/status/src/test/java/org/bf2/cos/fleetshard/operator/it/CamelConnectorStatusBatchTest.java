33f722018395db4d506de26f9b0bdcfe9f0bdb64
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-07-08 11:25:40 +0200

    it: merge status tests

---
+++ cos-fleetshard-operator-it/status/src/test/java/org/bf2/cos/fleetshard/operator/it/CamelConnectorStatusBatchTest.java
@@ -0,0 +1,29 @@
+package org.bf2.cos.fleetshard.operator.it;
+
+import java.util.Map;
+
+import org.bf2.cos.fleetshard.operator.it.support.camel.CamelTestProfile;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+
+@QuarkusTest
+@TestProfile(CamelConnectorStatusBatchTest.Profile.class)
+public class CamelConnectorStatusBatchTest extends CamelConnectorStatusTestSupport {
+
+    @Test
+    public void managedCamelConnectorStatusIsReportedByBatchTask() {
+        managedCamelConnectorStatusIsReported();
+    }
+
+    public static class Profile extends CamelTestProfile {
+        @Override
+        public Map<String, String> getConfigOverrides() {
+            return Map.of(
+                "cos.connectors.status.sync.batch.interval", "5s",
+                "cos.connectors.status.sync.batch.enabled", "true",
+                "cos.connectors.status.sync.realtime.enabled", "false");
+        }
+    }
+}
