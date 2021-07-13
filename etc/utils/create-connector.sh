835908b454609e065781beed89594ed585e4ef6e
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-07-13 22:52:51 +0200

    util: add create-connector script

---
+++ etc/utils/create-connector.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+BASE=${BASE_PATH}/api/connector_mgmt/v1
+CONNECTORS_BASE=${BASE}/kafka_connectors
+
+curl --insecure --oauth2-bearer "$(ocm token)" -S -s -D /dev/stderr "${CONNECTORS_BASE}?async=true" -d "$@" | jq
