3bce971f47b0ba2a44cf254bcd9a7940bebf7d95
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-08-17 16:45:27 +0200

    chore: tweak ci

--- etc/scripts/build_images.sh
+++ etc/scripts/build_images.sh
@@ -1,36 +1,5 @@
 #!/bin/bash -ex
-#
-# Copyright (c) 2018 Red Hat, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
-# This script builds and deploys the OCM Sendgrid Service. In order to
-# work, it needs the following variables defined in the CI/CD configuration of
-# the project:
-#
-# IMAGE_REPO_USERNAME - The name of the robot account used to push images to
-# 'quay.io', for example 'openshift-unified-hybrid-cloud+jenkins'.
-#
-# IMAGE_REPO_PASSWORD - The token of the robot account used to push images to
-# 'quay.io'.
-#
-# The machines that run this script need to have access to internet, so that
-# the built images can be pushed to quay.io.
-#
-
-
-# Log in to the image registry:
 if [ -z "${IMAGE_REPO_USERNAME}" ]; then
   echo "The quay.io push user name hasn't been provided."
   echo "Make sure to set the IMAGE_REPO_USERNAME environment variable."
@@ -70,4 +39,4 @@ fi
   -Dquarkus.container-image.additional-tags="${ADDITIONAL_TAGS}" \
   -Dquarkus.container-image.group=${IMAGE_REPO_NAMESPACE} \
   -Pcontainer-push \
-  -pl :cos-fleetshard-operator
+  -pl :"${1}"
