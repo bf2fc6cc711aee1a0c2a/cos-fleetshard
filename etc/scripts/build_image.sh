9b32ae6b8ec3f049ccf437815c121792dd43d4a5
Luis Garcia Acosta <lgarciaac@gmail.com>
2021-08-31 18:47:25 +0200

    Define platform for images (#83)
    * [chore] Define platform for images

--- etc/scripts/build_image.sh
+++ etc/scripts/build_image.sh
@@ -1,42 +1,37 @@
 #!/bin/bash -ex
 
-if [ -z "${IMAGE_REPO_USERNAME}" ]; then
-  echo "The quay.io push user name hasn't been provided."
-  echo "Make sure to set the IMAGE_REPO_USERNAME environment variable."
-  exit 1
-fi
+function print_exit() {
+    echo "Make sure to set the $1 environment variable."
+    exit 1
+}
 
-if [ -z "${IMAGE_REPO_PASSWORD}" ]; then
-  echo "The quay.io push token hasn't been provided."
-  echo "Make sure to set the IMAGE_REPO_PASSWORD environment variable."
-  exit 1
-fi
+for env_var in IMAGE_REPO_USERNAME IMAGE_REPO_PASSWORD IMAGE_REPO_NAMESPACE; do
+  [ -z "${!env_var}" ] && print_exit $env_var
+done
 
-if [ -z "${IMAGE_REPO_NAMESPACE}" ]; then
-  echo "The quay.io org hasn't been provided."
-  echo "Make sure to set the IMAGE_REPO_NAMESPACE environment variable."
-  exit 1
-fi
-
-export CONTAINER_REGISTRY_USR="${IMAGE_REPO_USERNAME}"
-export CONTAINER_REGISTRY_PWD="${IMAGE_REPO_PASSWORD}"
 export MAVEN_ARGS="-V -ntp -Dhttp.keepAlive=false -e"
 
 # CONTAINER_VERSION can be set to the release tag so an extra image is pushed
 # for the current build. When in CI, we also want a separate image for each commit
 [ -z "${CI}" ] || ADDITIONAL_TAGS="$(git log --pretty=format:'%h' -n 1)"
 
+# shellcheck disable=SC2236
 if [ ! -z "${CONTAINER_VERSION}" ]; then
-  [ -z ${ADDITIONAL_TAGS} ] && ADDITIONAL_TAGS="${CONTAINER_VERSION}" \
+  [ -z "${ADDITIONAL_TAGS}" ] && ADDITIONAL_TAGS="${CONTAINER_VERSION}" \
     || ADDITIONAL_TAGS="${CONTAINER_VERSION},${ADDITIONAL_TAGS}"
 fi
 
+QUARKUS_BASE_IMAGE="${QUARKUS_BASE_IMAGE:-adoptopenjdk/openjdk11:ubi-minimal}"
+QUARKUS_PLATFORM="${QUARKUS_PLATFORM:-linux/amd64}"
+
 ./mvnw ${MAVEN_ARGS} \
   clean package \
-  -Dquarkus.container-image.username="${CONTAINER_REGISTRY_USR}" \
-  -Dquarkus.container-image.password="${CONTAINER_REGISTRY_PWD}" \
+  -Dquarkus.jib.base-jvm-image="${QUARKUS_BASE_IMAGE}" \
+  -Dquarkus.jib.platforms="${QUARKUS_PLATFORM}" \
+  -Dquarkus.container-image.username="${IMAGE_REPO_USERNAME}" \
+  -Dquarkus.container-image.password="${IMAGE_REPO_PASSWORD}" \
   -Dquarkus.container-image.tag=latest \
   -Dquarkus.container-image.additional-tags="${ADDITIONAL_TAGS}" \
-  -Dquarkus.container-image.group=${IMAGE_REPO_NAMESPACE} \
+  -Dquarkus.container-image.group="${IMAGE_REPO_NAMESPACE}" \
   -Pcontainer-push \
   -pl :"${1}"
