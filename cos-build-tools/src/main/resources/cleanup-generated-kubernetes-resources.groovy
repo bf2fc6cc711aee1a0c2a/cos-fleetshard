8856c9dda30cc70a65f6a461a3778b48f966c363
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-09-29 16:50:44 +0200

    chore: sanitize generate kubernetes resources

---
+++ cos-build-tools/src/main/resources/cleanup-generated-kubernetes-resources.groovy
@@ -0,0 +1,22 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator
+import io.fabric8.kubernetes.api.model.GenericKubernetesResource
+
+def factory = new YAMLFactory().disable(YAMLGenerator.Feature.USE_NATIVE_TYPE_ID)
+def mapper = new ObjectMapper(factory).configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true)
+def parser = factory.createParser(inputFile as File)
+var docs = mapper.readValues(parser, GenericKubernetesResource.class).readAll()
+
+docs.each {
+    it?.metadata?.annotations?.remove('app.quarkus.io/commit-id')
+    it?.metadata?.annotations?.remove('app.quarkus.io/build-timestamp')
+
+    it?.additionalProperties?.spec?.template?.metadata?.annotations?.remove('app.quarkus.io/commit-id')
+    it?.additionalProperties?.spec?.template?.metadata?.annotations?.remove('app.quarkus.io/build-timestamp')
+}
+
+(outputFile as File).withWriter {
+    mapper.writer().writeValues(it).writeAll(docs)
+}
