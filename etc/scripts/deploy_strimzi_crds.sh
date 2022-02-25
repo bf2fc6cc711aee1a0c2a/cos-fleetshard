ee96492b70413a390bb56302e6f673cea6969165
Luca Burgazzoli <lburgazzoli@gmail.com>
2022-02-25 17:56:48 +0100

    chore: update crds to strimzi 0.28.0

--- etc/scripts/deploy_strimzi_crds.sh
+++ etc/scripts/deploy_strimzi_crds.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
-kubectl apply -f https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/0.27.0/install/cluster-operator/041-Crd-kafkaconnect.yaml
-kubectl apply -f https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/0.27.0/install/cluster-operator/047-Crd-kafkaconnector.yaml
+kubectl apply -f https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/0.28.0/install/cluster-operator/041-Crd-kafkaconnect.yaml
+kubectl apply -f https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/0.28.0/install/cluster-operator/047-Crd-kafkaconnector.yaml
