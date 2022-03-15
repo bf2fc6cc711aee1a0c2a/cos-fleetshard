69b57ecf3f77cc83a0c950ceefd7a94022d65c1f
rkerner <rkerner.mobil@gmail.com>
2022-03-15 15:49:47 +0100

    MGDCTRS-602 Bugfixes: pull secret renaming, NPE when data_shape is not set, Strimzi CrashBackLoops * cos-pull-secret renamed to addon-pullsecret * fix NPE when data_shape is not set * stop CrashBackLoops for Debezium by setting restartPolicy for Strimzi Kafka Connect pods to Never
    closes https://issues.redhat.com/browse/MGDCTRS-602

--- cos-fleetshard-operator-debezium/src/main/java/org/bf2/cos/fleetshard/operator/debezium/model/KeyAndValueConverters.java
+++ cos-fleetshard-operator-debezium/src/main/java/org/bf2/cos/fleetshard/operator/debezium/model/KeyAndValueConverters.java
@@ -6,9 +6,12 @@ import java.util.stream.Collectors;
 
 import org.bf2.cos.fleetshard.api.ManagedConnector;
 import org.bf2.cos.fleetshard.api.ServiceAccountSpec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class KeyAndValueConverters {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(KeyAndValueConverters.class);
     public static final String PROPERTY_KEY_CONVERTER = "key.converter";
     public static final String PROPERTY_VALUE_CONVERTER = "value.converter";
 
@@ -19,6 +22,13 @@ public class KeyAndValueConverters {
 
     public static Map<String, Object> getConfig(DebeziumDataShape dataShape, ManagedConnector config,
         ServiceAccountSpec serviceAccountSpec) {
+        if (null == dataShape) {
+            LOGGER.error("Missing `data_shape` config in Debezium ManagedConnector \"" + config.getMetadata().getName()
+                + "\"! Falling back to default.");
+            dataShape = new DebeziumDataShape()
+                .setKeyConverter(DebeziumDataShape.DEFAULT_CONVERTER_TYPE_KEY)
+                .setValueConverter(DebeziumDataShape.DEFAULT_CONVERTER_TYPE_VALUE);
+        }
         var converterConfig = new HashMap<>(Map.of(
             PROPERTY_KEY_CONVERTER, dataShape.getKeyConverter().getConverterClass(),
             PROPERTY_VALUE_CONVERTER, dataShape.getValueConverter().getConverterClass()));
