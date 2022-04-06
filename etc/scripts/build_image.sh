9a1e3704a3d94a3da9a4fe23e29af9ad45f50d54
Luca Burgazzoli <lburgazzoli@gmail.com>
2022-04-06 09:19:39 +0200

    ci: configure image tags from github ref

--- etc/scripts/build_image.sh
+++ etc/scripts/build_image.sh
@@ -7,14 +7,25 @@ function print_exit() {
 
 [ "$#" -eq 1 ] || print_exit "This script needs one parameter."
 
-for env_var in IMAGE_REPO_USERNAME IMAGE_REPO_PASSWORD IMAGE_VERSION; do
+for env_var in IMAGE_REPO_USERNAME IMAGE_REPO_PASSWORD IMAGE_TAG; do
   [ -z "${!env_var}" ] && print_exit "Make sure to set the ${env_var} environment variable."
 done
 
-./mvnw ${MAVEN_ARGS} \
-  clean package \
-  -Dquarkus.container-image.username="${IMAGE_REPO_USERNAME}" \
-  -Dquarkus.container-image.password="${IMAGE_REPO_PASSWORD}" \
-  -Dquarkus.container-image.tag="${IMAGE_VERSION}" \
-  -Pcontainer-push \
-  -pl :"${1}"
+if [ -z "${IMAGE_ADDITIONAL_TAGS}" ]; then
+  ./mvnw ${MAVEN_ARGS} \
+    clean package \
+      -Dquarkus.container-image.username="${IMAGE_REPO_USERNAME}" \
+      -Dquarkus.container-image.password="${IMAGE_REPO_PASSWORD}" \
+      -Dquarkus.container-image.tag="${IMAGE_TAG}" \
+      -Pcontainer-push \
+      -pl :"${1}"
+else
+  ./mvnw ${MAVEN_ARGS} \
+    clean package \
+      -Dquarkus.container-image.username="${IMAGE_REPO_USERNAME}" \
+      -Dquarkus.container-image.password="${IMAGE_REPO_PASSWORD}" \
+      -Dquarkus.container-image.tag="${IMAGE_TAG}" \
+      -Dquarkus.container-image.additional-tags="${IMAGE_ADDITIONAL_TAGS}" \
+      -Pcontainer-push \
+      -pl :"${1}"
+fi
