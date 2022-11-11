dccefb68aa50eba1d07b68771c4b95ebaf359f67
Andrea Tarocchi <andrea.tarocchi@gmail.com>
2022-11-11 18:45:14 +0100

    MGDCTRS-1598: Modify cos-fleetshard-sync, cos-fleetshard-operator-camel and cos-fleetshard-operator-debezium to make it possible to change log lever to DEBUG This commit actually enables a configmap based override sysetem that can be used to change any application property not just the log level.

---
+++ cos-fleetshard-support/src/main/java/org/bf2/cos/fleetshard/support/config/ApplicationOverrideConfigSourceProvider.java
@@ -0,0 +1,43 @@
+package org.bf2.cos.fleetshard.support.config;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.microprofile.config.spi.ConfigSource;
+import org.eclipse.microprofile.config.spi.ConfigSourceProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.smallrye.config.PropertiesConfigSource;
+
+public class ApplicationOverrideConfigSourceProvider implements ConfigSourceProvider {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationOverrideConfigSourceProvider.class);
+    private static final String OVERRIDE_PROPERTIES_LOCATION = "OVERRIDE_PROPERTIES_LOCATION";
+
+    @Override
+    public Iterable<ConfigSource> getConfigSources(ClassLoader forClassLoader) {
+        String overridePropertiesLocation = System.getenv(OVERRIDE_PROPERTIES_LOCATION);
+        if (overridePropertiesLocation == null) {
+            LOGGER.info("Properties Override support is disabled since OVERRIDE_PROPERTIES_LOCATION env var was not set.");
+            return Collections.EMPTY_LIST;
+        }
+        File f = new File(overridePropertiesLocation);
+        if (f.exists() && !f.isDirectory()) {
+            try {
+                return List
+                    .of(new PropertiesConfigSource(f.toURI().toURL(), ConfigSource.DEFAULT_ORDINAL + 1000));
+            } catch (IOException e) {
+                throw new RuntimeException(
+                    "OVERRIDE_PROPERTIES_URL config var had value " + overridePropertiesLocation
+                        + "that is not a valid url according to new File(overridePropertiesLocation).toURI().toURL().",
+                    e);
+            }
+        } else {
+            LOGGER.warn("OVERRIDE_PROPERTIES_LOCATION env var refer to a location not existent or that is not a file: {}",
+                overridePropertiesLocation);
+        }
+        return Collections.EMPTY_LIST;
+    }
+}
